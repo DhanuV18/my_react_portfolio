cafb4eb507be37a2ff35aed3ddec7de288f621b6
Dhanu <dhanushan.info@gmail.com>
2023-03-01 23:16:54 +0530

    testing 02

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -49,13 +49,13 @@ const About = () => {
             <div className="about__details">
               <div >
                 <h2 className="name__heading">Who is Dhanushan?</h2>
-                <p className="paragraph__one">{aboutMe.paragraph1}</p>
+                {/* <p className="paragraph__one">{aboutMe.paragraph1}</p> */}
 
                   <motion.div
             whileInView={{ y: [-150, 0], opacity: [0, 1] }}
             transition={{ duration: 0.3 }}
           >
-                <p className="paragraph__two">{aboutMe.paragraph2}</p>
+                {/* <p className="paragraph__two">{aboutMe.paragraph2}</p> */}
            </motion.div> 
                 <div className="CV__details">
                   <h3 src="#contact" className="contact__link">
