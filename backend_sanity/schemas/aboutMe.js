ad7a46b1c90fafd438f5903149d4e97afd0abc1e
Dhanu <dhanushan.info@gmail.com>
2023-03-01 22:56:49 +0530

    backend added into frontend

---
+++ backend_sanity/schemas/aboutMe.js
@@ -0,0 +1,18 @@
+export default {
+    name:'aboutMe',
+    title:'AboutMe',
+    type: 'document',
+    fields:[
+        { 
+            name:'paragraph1',
+            title:'Paragraph1',
+            type: 'string'
+        },
+        
+        { 
+            name:'paragraph2',
+            title:'Paragraph2',
+            type: 'string'
+        }
+    ]
+}
---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,20 @@
+export const schemaTypes = [
+    aboutMe,
+    abouts,
+    // brands,
+    contact,
+    // experiences,
+    skills,
+    // workExperience,
+    works
+]
+
+// import testimonials from "./about_me"
+import abouts from "./abouts"
+// import brands from "./brands"
+import contact from "./contact"
+// import experiences from "./experiences"
+import skills from "./skills"
+// import workExperience from "./workExperience"
+import works from "./works"
+import aboutMe from "./aboutMe"
